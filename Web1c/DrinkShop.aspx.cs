1fa62ec919368e0995e42f46753baa0a45d3a744
OtomeSound <s890081tonyhsu@gmail.com>
2016-11-07 00:14:18 +0800

    RowDelete is working, but RowEditing isn't. 1. Use SqlDataAdapter to control dataset and gridview. 2. Use Nlog to analyze the error Handling.

--- Web1c/DrinkShop.aspx.cs
+++ Web1c/DrinkShop.aspx.cs
@@ -6,15 +6,17 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
+using NLog;
 
 namespace Web1c
 {
     public partial class DrinkShop : System.Web.UI.Page
     {
-        
-        DataSet ds = null;
-        SqlConnection Conn = null;
-        static string Web1cConnectionString = "Data Source=127.0.0.1,9061;Initial Catalog=Web1c;Integrated Security=True";
+        const string Web1cConnectionString = "Data Source=127.0.0.1,9061;Initial Catalog=Web1c;Integrated Security=True";
+        DataSet ds = new DataSet();
+        SqlDataAdapter da = new SqlDataAdapter(); // for OrderDrinks
+        SqlConnection Conn = new SqlConnection(Web1cConnectionString);
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
         protected void Page_Load(object sender, EventArgs e)
         {
             if(Session["User_Account"] == null) Response.Redirect("./Default.aspx");
@@ -22,11 +24,9 @@ namespace Web1c
             SessionPoints.Text = "目前你還有" + Session["User_Points"] + "元";
             try
             {
-                Conn = new SqlConnection(Web1cConnectionString);
                 Conn.Open();
 
                 SqlDataAdapter da = new SqlDataAdapter("SELECT drink_id, drink_name, drink_price FROM Drinks ORDER BY drink_id", Conn);
-                ds = new DataSet();
                 da.Fill(ds, "Drinks");
                 ds.Tables["Drinks"].PrimaryKey = new DataColumn[] { ds.Tables["Drinks"].Columns["drink_id"] };
 
@@ -43,6 +43,7 @@ namespace Web1c
             }
             catch (SqlException sqlE)
             {
+                logger.Error(sqlE);
                 Response.Write("<script>window.alert('飲料清單讀取失敗')</script>");
             }
             finally
@@ -78,7 +79,6 @@ namespace Web1c
         {
             try
             {
-                Conn = new SqlConnection(Web1cConnectionString);
                 Conn.Open();
                 SqlCommand cmd = new SqlCommand("INSERT INTO [Orders]( order_time, order_user_pass) VALUES( GETDATE(), @order_user_pass);", Conn);
                 cmd.Parameters.AddWithValue("@order_user_pass", Session["User_Password"].ToString());
@@ -99,63 +99,66 @@ namespace Web1c
             }
             catch (SqlException sqlE)
             {
+                logger.Error(sqlE);
                 Response.Write("<script>window.alert('訂單建立失敗')</script>");
             }
             finally
             {
                 Conn.Close();
-                OrderDrinks_Item_View();
+                OrderDrinks_Item_Dataset();
             }
         }
-        protected void OrderDrinks_Item_View()
+        protected void OrderDrinks_Item_Dataset()
         {
             try
             {
-                Conn = new SqlConnection(Web1cConnectionString);
                 Conn.Open();
-
-                SqlDataAdapter da = new SqlDataAdapter("SELECT Drinks.drink_name, OrderDrinks.orderdrink_no, OrderDrinks.orderdrink_sweet, OrderDrinks.orderdrink_ice, Drinks.drink_price * OrderDrinks.orderdrink_no AS orderdrink_total_price FROM Drinks INNER JOIN OrderDrinks ON Drinks.drink_id = OrderDrinks.orderdrink_drink_id WHERE OrderDrinks.orderdrink_order_id = @orderdrink_order_id", Conn); // 從OrderDrinks和Drinks抓資料來組合
+                da.SelectCommand = new SqlCommand("SELECT OrderDrinks.orderdrink_count, OrderDrinks.orderdrink_drink_id, OrderDrinks.orderdrink_no, OrderDrinks.orderdrink_sweet, OrderDrinks.orderdrink_ice, Drinks.drink_price * OrderDrinks.orderdrink_no AS orderdrink_total_price FROM Drinks INNER JOIN OrderDrinks ON Drinks.drink_id = OrderDrinks.orderdrink_drink_id WHERE OrderDrinks.orderdrink_order_id = @orderdrink_order_id", Conn);
                 da.SelectCommand.Parameters.AddWithValue("@orderdrink_order_id", Session["order_id"]);
-                ds = new DataSet();
-
                 da.Fill(ds, "OrderDrinks_with_Price"); //執行select
-                OrderDrinks_total_label.Text = "共有" + ds.Tables["OrderDrinks_with_Price"].Rows.Count + "筆資料";
-                OrderDrinks_GridView.DataSource = ds;
-                OrderDrinks_GridView.DataMember = "OrderDrinks_with_Price";
-                OrderDrinks_GridView.DataBind();
+                ds.Tables["OrderDrinks_with_Price"].PrimaryKey = new DataColumn[] { ds.Tables["OrderDrinks_with_Price"].Columns["orderdrink_count"] };
             }
             catch (SqlException sqlE)
             {
+                logger.Error(sqlE);
                 Response.Write("<script>window.alert('項目查詢失敗')</script>");
             }
             finally
             {
-                Conn.Close();
+                Conn.Close();                
             }
+        }
+        protected void OrderDrinks_Item_GridViewSet()
+        {
+            OrderDrinks_total_label.Text = "共有" + ds.Tables["OrderDrinks_with_Price"].Rows.Count + "筆資料";
+            OrderDrinks_GridView.DataSource = ds;
+            OrderDrinks_GridView.DataMember = "OrderDrinks_with_Price";
+            OrderDrinks_GridView.DataBind();            
         }
         protected void OrderDrinks_Item_Insert_Button_Click(object sender, EventArgs e)
         {
             try
             {
-                Conn = new SqlConnection(Web1cConnectionString);
                 Conn.Open();
-                SqlCommand cmd = new SqlCommand("INSERT INTO [OrderDrinks](orderdrink_order_id, orderdrink_no, orderdrink_drink_id, orderdrink_sweet, orderdrink_ice) VALUES (@orderdrink_order_id, @orderdrink_no, @orderdrink_drink_id, @orderdrink_sweet, @orderdrink_ice)", Conn);
-                cmd.Parameters.AddWithValue("@orderdrink_order_id", Session["order_id"]);
-                cmd.Parameters.AddWithValue("@orderdrink_no", OrderDrinks_no_dropdown.SelectedValue);
-                cmd.Parameters.AddWithValue("@orderdrink_drink_id", DrinkName_Dropdown.SelectedValue);
-                cmd.Parameters.AddWithValue("@orderdrink_sweet", OrderDrinks_sweet_dropdown.SelectedValue);
-                cmd.Parameters.AddWithValue("@orderdrink_ice", OrderDrinks_ice_dropdown.SelectedValue);
+                da.InsertCommand = new SqlCommand("INSERT INTO [OrderDrinks](orderdrink_order_id, orderdrink_no, orderdrink_drink_id, orderdrink_sweet, orderdrink_ice) VALUES (@orderdrink_order_id, @orderdrink_no, @orderdrink_drink_id, @orderdrink_sweet, @orderdrink_ice)", Conn);
+                da.InsertCommand.Parameters.AddWithValue("@orderdrink_order_id", Session["order_id"]);
+                da.InsertCommand.Parameters.AddWithValue("@orderdrink_no", OrderDrinks_no_dropdown.SelectedValue);
+                da.InsertCommand.Parameters.AddWithValue("@orderdrink_drink_id", DrinkName_Dropdown.SelectedValue);
+                da.InsertCommand.Parameters.AddWithValue("@orderdrink_sweet", OrderDrinks_sweet_dropdown.SelectedValue);
+                da.InsertCommand.Parameters.AddWithValue("@orderdrink_ice", OrderDrinks_ice_dropdown.SelectedValue);
 
-                cmd.ExecuteNonQuery();
+                da.InsertCommand.ExecuteNonQuery();
             }
             catch (SqlException sqlE)
             {
+                logger.Error(sqlE);
                 Response.Write("<script>window.alert('項目新增失敗 "+sqlE+"')</script>");
             }
             finally
             {
                 Conn.Close();
-                OrderDrinks_Item_View();
+                OrderDrinks_Item_Dataset();
+                OrderDrinks_Item_GridViewSet();
             }
         }
         protected string formatColumnData(string type, int n)
@@ -166,6 +169,11 @@ namespace Web1c
             if (n == -1) return res;
             if (type == "sweet") return sweet[n];
             if (type == "ice") return ice[n];
+            if (type == "name")
+            {
+                DataRow rowFind = ds.Tables["Drinks"].Rows.Find(n);
+                return rowFind.Field<string>("drink_name");
+            }
             return res;
         }
 
@@ -173,7 +181,6 @@ namespace Web1c
         {
             try
             {
-                Conn = new SqlConnection(Web1cConnectionString);
                 Conn.Open();
                 SqlCommand cmd = new SqlCommand("UPDATE Orders SET order_status = 2 WHERE order_id = @order_id AND order_user_pass = @order_user_pass", Conn);
                 cmd.Parameters.AddWithValue("@order_id", Session["order_id"].ToString());
@@ -188,6 +195,7 @@ namespace Web1c
             }
             catch (SqlException sqlE)
             {
+                logger.Error(sqlE);
                 Response.Write("<script>window.alert('訂單確認失敗')</script>");
             }
             finally
@@ -200,7 +208,6 @@ namespace Web1c
         {
             try
             {
-                Conn = new SqlConnection(Web1cConnectionString);
                 Conn.Open();
                 SqlCommand cmd = new SqlCommand("UPDATE Orders SET order_status = 0 WHERE order_id = @order_id AND order_user_pass = @order_user_pass", Conn);
                 cmd.Parameters.AddWithValue("@order_id", Session["order_id"].ToString());
@@ -215,6 +222,7 @@ namespace Web1c
             }
             catch (SqlException sqlE)
             {
+                logger.Error(sqlE);
                 Response.Write("<script>window.alert('訂單取消失敗')</script>");
             }
             finally
@@ -225,27 +233,109 @@ namespace Web1c
 
         protected void OrderDrinks_Gridview_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
+            try
+            {
+                OrderDrinks_Item_Dataset();
+                logger.Info("Index at: "+Convert.ToInt32(e.RowIndex));
+                string orderdrink_count = ds.Tables["OrderDrinks_with_Price"].Rows[e.RowIndex]["orderdrink_count"].ToString();
+                Conn.Open();
+                da.DeleteCommand = new SqlCommand("DELETE FROM OrderDrinks WHERE orderdrink_count = @orderdrink_count", Conn);
+                da.DeleteCommand.Parameters.AddWithValue("@orderdrink_count", orderdrink_count);
 
+                ds.Tables["OrderDrinks_with_Price"].Rows[e.RowIndex].Delete();
+                da.Update(ds, "OrderDrinks_with_Price");
+            }
+            catch (SqlException sqlE)
+            {
+                logger.Error(sqlE);
+                Response.Write("<script>window.alert('項目刪除失敗')</script>");
+            }
+            finally
+            {
+                Conn.Close();
+                OrderDrinks_Item_GridViewSet();
+                logger.Info("At deleting, OrderDrinks_with_Price has " + ds.Tables["OrderDrinks_with_Price"].Rows.Count + " rows.");
+            }
+        }
+
+        protected void OrderDrinks_Gridview_RowDeleted(object sender, GridViewDeletedEventArgs e)
+        {
+            logger.Info("At deleted, OrderDrinks_with_Price has " + ds.Tables["OrderDrinks_with_Price"].Rows.Count + " rows.");
+            OrderDrinks_Item_Dataset();
+            OrderDrinks_Item_GridViewSet();
         }
 
         protected void OrderDrinks_Gridview_RowEditing(object sender, GridViewEditEventArgs e)
         {
-
-        }
-
-        protected void OrderDrinks_Gridview_RowUpdating(object sender, GridViewUpdateEventArgs e)
-        {
-
-        }
-
-        protected void OrderDrinks_Gridview_RowDataBound(object sender, GridViewRowEventArgs e)
-        {
-
+            OrderDrinks_GridView.EditIndex = e.NewEditIndex;
+            OrderDrinks_Item_Dataset();
         }
 
         protected void OrderDrinks_Gridview_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
         {
+            OrderDrinks_GridView.EditIndex = -1;
+            OrderDrinks_Item_Dataset();
+        }
 
+        protected void OrderDrinks_Gridview_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = OrderDrinks_GridView.Rows[e.RowIndex];
+                DropDownList orderdrink_drink_id, orderdrink_no, orderdrink_sweet, orderdrink_ice;
+                orderdrink_drink_id = (row.FindControl("OrderDrink_Template_drink_name_dropdown") as DropDownList);
+                orderdrink_no = (row.FindControl("OrderDrink_Template_no_dropdown") as DropDownList);
+                orderdrink_sweet = (row.FindControl("OrderDrink_Template_sweet_dropdown") as DropDownList);
+                orderdrink_ice = (row.FindControl("OrderDrink_Template_ice_dropdown") as DropDownList);
+
+                OrderDrinks_Item_Dataset();
+                logger.Info("Index at: "+Convert.ToInt32(e.RowIndex));
+                string orderdrink_count = ds.Tables["OrderDrinks_with_Price"].Rows[e.RowIndex]["orderdrink_count"].ToString();
+                Conn.Open();
+                da.UpdateCommand = new SqlCommand("UPDATE OrderDrinks SET orderdrink_no = @orderdrink_no, orderdrink_drink_id = @orderdrink_drink_id, orderdrink_sweet = @orderdrink_sweet, orderdrink_ice = @orderdrink_ice WHERE orderdrink_count = @orderdrink_count", Conn);
+                da.UpdateCommand.Parameters.AddWithValue("@orderdrink_count", orderdrink_count);
+                da.UpdateCommand.Parameters.AddWithValue("@orderdrink_drink_id", orderdrink_drink_id.SelectedValue.ToString());
+                da.UpdateCommand.Parameters.AddWithValue("@orderdrink_no", orderdrink_no.SelectedValue.ToString());
+                da.UpdateCommand.Parameters.AddWithValue("@orderdrink_sweet", orderdrink_sweet.SelectedValue.ToString());
+                da.UpdateCommand.Parameters.AddWithValue("@orderdrink_ice", orderdrink_ice.SelectedValue.ToString());
+
+                ds.Tables["OrderDrinks_with_Price"].Rows[e.RowIndex]["orderdrink_drink_id"] = orderdrink_drink_id.SelectedValue.ToString();
+                ds.Tables["OrderDrinks_with_Price"].Rows[e.RowIndex]["orderdrink_no"] = orderdrink_no.SelectedValue.ToString();
+                ds.Tables["OrderDrinks_with_Price"].Rows[e.RowIndex]["orderdrink_sweet"] = orderdrink_sweet.SelectedValue.ToString();
+                ds.Tables["OrderDrinks_with_Price"].Rows[e.RowIndex]["orderdrink_ice"] = orderdrink_ice.SelectedValue.ToString();
+
+                da.Update(ds, "OrderDrinks_with_Price");
+            }
+            catch (SqlException sqlE)
+            {
+                logger.Error(sqlE);
+                Response.Write("<script>window.alert('選項更改失敗')</script>");
+            }
+            finally
+            {
+                Conn.Close();
+                OrderDrinks_Item_Dataset();
+            }
+        }
+
+        protected void OrderDrinks_Gridview_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if ((e.Row.RowState & DataControlRowState.Edit) > 0)
+                {
+                    DropDownList orderdrink_template_drink_id_dropdown = (DropDownList)OrderDrinks_GridView.Rows[e.Row.DataItemIndex].FindControl("OrderDrink_Template_drink_id_dropdown");
+                    // bind DropDown manually
+                    orderdrink_template_drink_id_dropdown.DataValueField = "drink_id";
+                    orderdrink_template_drink_id_dropdown.DataTextField = "drink_name";
+
+                    orderdrink_template_drink_id_dropdown.DataSource = ds.Tables["Drinks"].DefaultView;
+                    orderdrink_template_drink_id_dropdown.DataBind();
+                    var item = new ListItem("請選擇飲料", "0");
+                    orderdrink_template_drink_id_dropdown.Items.Insert(0, item);
+                    orderdrink_template_drink_id_dropdown.SelectedValue = e.Row.DataItem.ToString(); // you can use e.Row.DataItem to get the value
+                }
+            }
         }
     }
 }
